b672c56b9d5b9fe1cd434b70d8d8c1c0f01e555e
federicomolino <federicomolino450@gmail.com>
2025-01-25 09:51:15 +0100

    fiX Ciclo For, casistica in cui nome != messa fuori, inserito nuovo ciclo while, senza variabile booleana

--- src/CheckGuest.java
+++ src/CheckGuest.java
@@ -5,21 +5,24 @@ public class CheckGuest {
     public static void main(String[] args) throws Exception {
         String [] invitati = {"Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardis Zarei","Martina Maccherone","Rachel Zeilic"};
 
+        //String [] inviatiFesta = new String[4];
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Inserisci il tuo nome per poter entrare alla festa: ");
         String nome = scan.nextLine();
-        
+
+
         /*for (int i = 0; i < invitati.length; i++) {
             //Verifico se nella posizione in cui si trova il nome nell'array è uguale a quanto inserito dall'utente
             if (invitati[i].equals(nome)) {
                 System.out.println("Ciao " + nome +", puoi entrare");
-            } else {
-                System.out.println("Ciao " + nome +", mi dispiace ma il tuo nome non è in lista");
-                break;
+                return;
             }
-        }*/
+        }
 
-        int i = 0;
+        System.out.println("Ciao " + nome +", mi dispiace ma il tuo nome non è in lista"); 
+
+        /*int i = 0;
         boolean trovato = false;
 
         //controllo tutti gli elementi dell'array
@@ -34,7 +37,19 @@ public class CheckGuest {
         if (trovato) {
             System.out.println("Ciao " + nome +", puoi entrare");
         }else {
-            System.out.println("Ciao " + nome +", mi dispiace ma il tuo nome non è in lista");
+            System.out.println("Ciao " + nome +", mi dispiace ma il tuo nome non è in lista"); 
+        }*/
+
+        int i = 0;
+
+        while (i< invitati.length) {
+            if (invitati[i].equals(nome)) {
+                System.out.println("Ciao " + nome +", puoi entrare");
+                return;
+            }
+            i++;
         }
+        System.out.println("Ciao " + nome +", mi dispiace ma il tuo nome non è in lista"); 
+
     }
 }
